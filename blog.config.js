f5a16f16a1437ab105f517409130d3bbd8964aed
judeyang <byoox@qq.com>
2023-12-24 23:09:56 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'TheJoyousScience', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '🌏地球Online骨灰级玩家', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://judeyang.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '地球Online, Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '地球Online, Notion, 博客,  judeyang', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
